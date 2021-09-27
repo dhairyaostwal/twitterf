0e234fe14102475a814b9f89da8dd6fcee9cd8ef
Preet Patel <preetpatel@Preets-MacBook-Air.local>
2021-09-27 10:25:34 +0530

    frontend

---
+++ Frontend/home.js
@@ -0,0 +1,23 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+const showpop = (n) => {
+  document.getElementById(n).style.display="flex";
+}
+const hidepop = (n) => {
+  document.getElementById(n).style.display="none";
+}
+const shownav = () => {
+  document.getElementById("rightnav").style.display="flex";
+}
+const hidenav = () => {
+  document.getElementById("rightnav").style.display="none";
+}
